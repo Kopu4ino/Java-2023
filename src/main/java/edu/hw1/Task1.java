db9317f6b5d3c060e36bfb0ef081569d122206f0
Kopu4ino <avainglorry@gmail.com>
2023-10-04 18:17:54 +0500

    Снова исправление CodeStyle

--- src/main/java/edu/hw1/Task1.java
+++ src/main/java/edu/hw1/Task1.java
@@ -3,6 +3,10 @@ package edu.hw1;
 @SuppressWarnings("MagicNumber")
 public class Task1 {
 
+    private Task1(){
+
+    }
+
     public static int getSecDuration(String strDuration) {
         String regex = "^[0-9]+:[0-5][0-9]$";
 
@@ -10,9 +14,9 @@ public class Task1 {
             return -1;
         }
 
-        String[] arr_dur = strDuration.split(":");
-        int minutes = Integer.parseInt(arr_dur[0]);
-        int seconds = Integer.parseInt(arr_dur[1]);
+        String[] arrDur = strDuration.split(":");
+        int minutes = Integer.parseInt(arrDur[0]);
+        int seconds = Integer.parseInt(arrDur[1]);
 
         return 60 * minutes + seconds;
     }
