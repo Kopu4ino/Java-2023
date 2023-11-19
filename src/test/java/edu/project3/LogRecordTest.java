f1b6001f3322b5403be92710deb64e50e0878d66
Kopu4ino <avainglorry@gmail.com>
2023-11-19 23:31:58 +0500

    Corrected LogRecordTest

--- src/test/java/edu/project3/LogRecordTest.java
+++ src/test/java/edu/project3/LogRecordTest.java
@@ -1,46 +1,39 @@
 package edu.project3;
 
-import org.junit.jupiter.api.Test;
-
 import java.time.OffsetDateTime;
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+import java.time.ZoneOffset;
+import java.util.Optional;
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
 
-class LogRecordTest {
+public class LogRecordTest {
 
     @Test
-    void parseLogRecordCorrectInput() {
-        String logLine = "204.77.168.241 - - " +
-            "[17/May/2015:21:05:43 +0000] " +
-            "\"GET " +
-            "/downloads/product_2 " +
-            "HTTP/1.1\" " +
-            "404 " +
-            "338 " +
-            "\"-\" " +
-            "\"Debian APT-HTTP/1.3 (0.8.16~exp12ubuntu10.7)\"";
-        OffsetDateTime parsedDate = LogRecord.parseTimeLocal("17/May/2015:21:05:43 +0000");
+    public void testParseValidLogRecord() {
+        String logLine =
+            "192.168.1.1 - user1 [01/Jan/2023:10:00:00 +0000] \"GET /index.html HTTP/1.1\" 200 1234 \"-\" \"Mozilla/5.0\"";
+        Optional<LogRecord> result = LogRecord.parseLogRecord(logLine);
 
-        LogRecord expect = new LogRecord(
-            "204.77.168.241",
-            "-",
-            parsedDate,
-            "GET",
-            "/downloads/product_2",
-            "HTTP/1.1",
-            404,
-            338,
-            "-",
-            "Debian APT-HTTP/1.3 (0.8.16~exp12ubuntu10.7)"
-        );
-
-        assertThat(LogRecord.parseLogRecord(logLine)).isEqualTo(expect);
+        assertThat(result).isPresent();
+        result.ifPresent(logRecord -> {
+            assertThat(logRecord.remoteAddr()).isEqualTo("192.168.1.1");
+            assertThat(logRecord.remoteUser()).isEqualTo("user1");
+            assertThat(logRecord.timeLocal()).isEqualTo(OffsetDateTime.of(2023, 1, 1, 10, 0, 0, 0, ZoneOffset.UTC));
+            assertThat(logRecord.requestType()).isEqualTo("GET");
+            assertThat(logRecord.requestPath()).isEqualTo("/index.html");
+            assertThat(logRecord.protocol()).isEqualTo("HTTP/1.1");
+            assertThat(logRecord.status()).isEqualTo(200);
+            assertThat(logRecord.bodyBytesSent()).isEqualTo(1234);
+            assertThat(logRecord.httpReferer()).isEqualTo("-");
+            assertThat(logRecord.userAgent()).isEqualTo("Mozilla/5.0");
+        });
     }
 
     @Test
-    void parseLogRecordIncorrectInput() {
-        String logLine1 = "204.77.168.241 - **********";
+    public void testParseInvalidLogRecord() {
+        String invalidLogLine = "invalid log line";
+        Optional<LogRecord> result = LogRecord.parseLogRecord(invalidLogLine);
 
-        assertThatThrownBy(() -> LogRecord.parseLogRecord(logLine1)).isInstanceOf(IllegalArgumentException.class);
+        assertThat(result).isEmpty();
     }
 }
